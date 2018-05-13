b3bf803143bec52ab588946e1f464d9e6caaeea9
rrrr98 <mugen.nikki22@gmail.com>
2018-05-13 17:46:57 +0700

    fitur sisa

--- -pemesanan-submit.php
+++ -pemesanan-submit.php
@@ -12,20 +12,36 @@
 		<?php
 			include('navbar/user-navbar.php'); 
 		?>
-		<div class='my-container'>
+		<div class='my-container centered-container'>
 			<?php
 				if($_SERVER['REQUEST_METHOD']=='POST'){
-                    if (isset($_SESSION['orderErrorId'])) {
-                        if ($_SESSION['orderErrorId']==4) {
-                            echo "<h2>PENDAFTARAN SUCCESS</h2>";
-                            unset($_SESSION['orderErrorId']);
-                        }
-                    }
+					if (isset($_SESSION['orderErrorId'])) {
+						if ($_SESSION['orderErrorId']==4) {
+							echo "<h2>PENDAFTARAN SUCCESS</h2>";
+							unset($_SESSION['orderErrorId']);
+						}
+					}
 				}else if($_SERVER['REQUEST_METHOD']=='GET'){
 					echo "<h2>SUDAH PERNAH MEMESAN</h2>";
 				}
+				$nowDate = date('Y-m-d');
+				$queryForUpdate = "SELECT idPenanganan, idSpesialisasi, dokter.idDokter, waktuTemu FROM pekerjaandokter INNER JOIN dokter 
+													ON pekerjaandokter.idDokter = dokter.idDokter
+													WHERE idPasien = '$_SESSION[id]' AND DATEDIFF(waktuTemu, '$nowDate') >= 0 AND sudahBertemu=0";
+				$result = $conn->query($queryForUpdate);
+				$row = $result->fetch_assoc();
+				echo mysqli_error($conn);
+				$id = $row['idPenanganan'];
+				$speciality = $row['idSpesialisasi'];
+				$dokter = $row['idDokter'];
+				$hari = $row['waktuTemu'];
+				$url = 'pemesanan.php?id='.$id.'&speciality='.$speciality.'&dokter='.$dokter.'&hari="'.$hari.'"';
 			?>
-            
-        </div>
+			<button class="menu-btn" style='padding:0px; margin:auto;'>
+				<a style='text-decoration:none; width:100%;' href='<?=$url?>' class='menu-btn'>
+					Edit
+				</a>       
+			</button>
+    </div>
 	</body>
 </html>
--- jadwal.php
+++ jadwal.php
@@ -1,6 +1,51 @@
 <?php
 	include ('connection/session.php');
 ?>
+<?php
+	echo "<script>";
+	$allSpecialityQuery = "SELECT *
+						   FROM spesialisasi
+						   ORDER BY idSpesialisasi";
+	$result = $conn->query($allSpecialityQuery);
+	
+	if($result){
+		$arrSpeciality = array();
+		echo "var arrSpeciality = new Array() \n";
+		echo "var arrData = new Array() \n";
+		echo "var arrDokterId = new Array() \n";
+		while($row =$result->fetch_array()){
+			$id = $row['idSpesialisasi'];
+			$nama = $row['namaSpesialisasi'];
+			$arrSpeciality[$id] = $nama; 
+			echo  "arrSpeciality[$id] = '$nama' \n";
+			echo  "arrData[$id] =new Array() \n";
+		}
+		$allDokternSpeciality = "SELECT tusers.nama as nama, dokter.idDokter as idDokter, dokter.idSpesialisasi as spesialisasi
+								 FROM dokter
+								 inner join  users as tusers on tusers.idUser = dokter.idUser";
+		$check = $conn->query($allDokternSpeciality);
+		while($row = $check->fetch_array()){
+			$id = $row['spesialisasi'];
+			$nama = $row['nama'];
+			$idDokter = $row['idDokter'];
+			echo "arrData[$id].push('$nama') \n";
+			echo "arrDokterId[$idDokter] = '$nama' \n";
+		}
+		$dayQuery = "SELECT idDokter, hari FROM jadwalpraktek";
+		$dayResult = $conn->query($dayQuery);
+		echo "var arrDay = new Array() \n";
+		if ($dayResult) {
+			while ($row=$dayResult->fetch_array()) {
+				$idDokter = $row['idDokter'];
+				$hari = $row['hari'];
+				echo "if (typeof arrDay[$idDokter] == 'undefined') {";
+				echo "arrDay[$idDokter] = new Array(); arrDay[$idDokter].push('$hari');}";
+				echo "else {arrDay[$idDokter].push('$hari');}";
+			}
+		}
+	}
+	echo "</script>";
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -21,46 +66,23 @@
 		<div class="my-container centered-container">
 			<h3 style='padding-bottom:10px;'>Lihat Jadwal Praktek Dokter</h3>
 			<div>
-				<form method='POST' action="">
-					<div class="input-box">
-						<p>Pilih Hari</p>
-						<select class='my-form' name="hari">
-							<option value="" disabled selected hidden>Pilih Hari</option>
-							<option value="1">Senin</option>
-							<option value="2">Selasa</option>
-							<option value="3">Rabu</option>
-							<option value="4">Kamis</option>
-							<option value="5">Jumat</option>
-							<option value="6">Sabtu</option>
-							<option value="7">Minggu</option>
-							<!-- belum bisa diaplikasikan -->
-						</select>
-					</div>
+				<form method='POST' action="jadwal.php">
 					<div class="input-box">
 						<p>Pilih Speciality</p>
-						<select class='my-form' name="speciality">
+						<select id='specJadwal' class='my-form' name="speciality">
 							<option value="" disabled selected hidden>Pilih Speciality</option>
-							
-							<?php
-								$sql = "SELECT * FROM spesialisasi";
-								$result = $conn->query($sql);
-								while ($row = $result->fetch_assoc()) {
-									echo '<option value="'.$row["idSpesialisasi"].'">'.$row["namaSpesialisasi"].'</option>';
-								}
-							?>
 						</select>
 					</div>
 					<div class="input-box">
 						<p>Pilih Dokter</p>
-						<select class='my-form' name="dokter">
+						<select id='dokterJadwal' class='my-form' name="dokter">
 							<option value="" disabled selected hidden>Pilih Dokter</option>
-							<?php
-								$sql = "SELECT * FROM dokter INNER JOIN users ON dokter.idUser = users.idUser";
-								$result = $conn->query($sql);
-								while ($row = $result->fetch_assoc()) {
-									echo '<option value="'.$row["idUser"].'">'.$row["nama"].'</option>';
-								}
-							?>
+						</select>
+					</div>
+					<div class="input-box">
+						<p>Pilih Hari</p>
+						<select id='hariJadwal' class='my-form' name="hari">
+							<option value="" disabled selected hidden>Pilih Hari</option>
 						</select>
 					</div>
 					<div class="container-menu-btn">
@@ -68,8 +90,74 @@
 							Check !!
 						</button>
 					</div>
+					<div>
+						<h4>
+							<?php
+								if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+									if (isset($_POST['speciality']) && isset($_POST['dokter']) && isset($_POST['hari'])) {
+										if (!is_null($_POST['speciality']) && !is_null($_POST['dokter']) && !is_null($_POST['hari'])) {
+											echo 'Jadwal ditemukan !';
+										}
+										else {
+											echo 'Jadwal tidak Ditemukan';
+										}
+									}
+									else {
+										echo 'Jadwal tidak Ditemukan';
+									}
+								}
+							?>
+						</h4>
+					</div>
 				</form>
 			</div>
 		</div>
 	</body>
 </html>
+
+<script>	
+	$(document).ready(function(){
+		var dktr = $("#dokterJadwal");
+		var mappingDay = [
+			"null",
+			"Senin",
+			"Selasa",
+			"Rabu",
+			"Kamis",
+			"Jumat",
+			"Sabtu",
+			"Minggu",
+		];
+		$('#specJadwal').on('change',function(){
+			$("#dokterJadwal").empty();
+			var dataArray = arrData[this.value];
+			$.each(dataArray,function(index,val){
+				for(var i = 0 ; i<arrDokterId.length;i++){
+					if(arrDokterId[i]==val){
+						dktr.append($("<option/>").attr("value",i).text(val));
+					}
+				}
+			});
+			$('#dokterJadwal').trigger('change');
+		});
+		var hari = $('#hariJadwal');
+		$('#dokterJadwal').on('change',function(){
+			$("#hariJadwal").empty();
+			var dataArray = arrDay[this.value];
+			$.each(dataArray,function(index,val){
+				hari.append($("<option/>").attr("value",val).text(mappingDay[val]));
+			});
+			console.log(dataArray);
+		});
+		var specMenu = $("#specJadwal");
+		$("#specJadwal option:gt(0)").empty();
+		$.each(arrSpeciality,function(index,value){
+			console.log('masuk');
+			if(index!=0){
+				if (value != null) {
+					specMenu.append($("<option/>").attr("value",index).text(value));
+				}
+			}
+		})
+	});
+</script>
